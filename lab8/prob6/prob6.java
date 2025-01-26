a0d393e10fe457468fcd615a77def111a3122385
Hcinet Amin <53219971+HcinetA@users.noreply.github.com>
2025-01-25 22:58:00 -0600

    lab8

---
+++ lab8/prob6/prob6.java
@@ -0,0 +1,18 @@
+package MPP.lab8.prob6;
+
+import MPP.lab8.prob2a.Employee;
+
+import java.util.function.Function;
+
+public class prob6 {
+
+
+    //A. (Employee e) -> e.getName()
+  //  Function<Employee, String> emp = e->e.getName();
+   // Function <Employee,String> emp1 = Employee::getName;
+
+    //B. (Employee e) -> e.getName()
+    //  Function<Employee, String> emp =(e,s)->e.getName();
+    // Function <Employee,String> emp1 = Employee::getName;
+
+}
